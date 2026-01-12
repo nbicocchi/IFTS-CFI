98cd68dd936ed65f70e46b48702583888af3a3e9
Nicola Bicocchi <nbicocchi@unimore.it>
2026-01-12 13:34:37 +0100

    update

--- code/movingpoint/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.movingpoint {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.movingpoint to javafx.fxml;
-    exports org.example.movingpoint;
-}
