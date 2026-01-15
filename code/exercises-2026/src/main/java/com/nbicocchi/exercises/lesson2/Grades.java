9878e829195e07caccd4f33f258a6ecf8c04b2c0
Nicola Bicocchi <nbicocchi@unimore.it>
2026-01-15 15:43:01 +0100

    update

--- code/exercises-2026/src/main/java/com/nbicocchi/exercises/lesson2/Grades.java
+++ code/exercises-2026/src/main/java/com/nbicocchi/exercises/lesson2/Grades.java
@@ -2,11 +2,11 @@ package com.nbicocchi.exercises.lesson2;
 
 public class Grades {
     public static String getWordforGrade(int grade) {
-        if (grade < 8) {
+        if (grade >= 8) {
             return "Ottimo";
-        } else if (grade > 6) {
+        } else if (grade >= 6) {
             return "Ok";
-        } else if (grade > 4) {
+        } else if (grade >= 4) {
             return "Scarso";
         } else  {
             return "Pessimo";
@@ -14,6 +14,7 @@ public class Grades {
     }
 
     public static void main(String[] args) {
-        System.out.println(getWordforGrade(2));
+        int grade = 6;
+         System.out.println(grade + " --> " + getWordforGrade(grade));
     }
 }
