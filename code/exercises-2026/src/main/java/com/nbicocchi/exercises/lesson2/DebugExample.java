c8c56fe05b32ace8afebd8321c1db747107d925a
Nicola Bicocchi <nbicocchi@unimore.it>
2026-01-15 12:19:24 +0100

    update

---
+++ code/exercises-2026/src/main/java/com/nbicocchi/exercises/lesson2/DebugExample.java
@@ -0,0 +1,12 @@
+package com.nbicocchi.exercises.lesson2;
+
+public class DebugExample {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int c = 4;
+        if (c > 4) {
+            System.out.println("ciccio");
+        }
+    }
+}
