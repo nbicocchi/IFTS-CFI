0a6caf1d78b86bfe4f72bdd5ed69637c56297f46
Nicola Bicocchi <nbicocchi@unimore.it>
2026-02-17 12:39:23 +0100

    update

---
+++ code/exercises-2026/src/main/java/com/nbicocchi/exercises/lesson10/Car.java
@@ -0,0 +1,41 @@
+package com.nbicocchi.exercises.lesson10;
+
+import java.util.Objects;
+
+public class Car {
+    Long id;
+    String brand;
+    String model;
+
+    public Car(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public Car(Long id, String brand, String model) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(id, car.id) && Objects.equals(brand, car.brand) && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, brand, model);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+}
